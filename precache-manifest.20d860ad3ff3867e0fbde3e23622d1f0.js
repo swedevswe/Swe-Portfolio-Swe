22c35b66a317b0adb97d08023f0bae60790a001a
Swetha Sebastian <swthsebastian@gmail.com>
2024-05-14 11:13:56 +0100

    Updates

---
+++ precache-manifest.20d860ad3ff3867e0fbde3e23622d1f0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c59f6674c83d048df4d232650ea879",
+    "url": "/Swe-Portfolio-Swe/index.html"
+  },
+  {
+    "revision": "10c8a3d03e1534f13df1",
+    "url": "/Swe-Portfolio-Swe/static/css/2.96c444d9.chunk.css"
+  },
+  {
+    "revision": "cbf326b96cb1d0bc2d95",
+    "url": "/Swe-Portfolio-Swe/static/css/main.567478d9.chunk.css"
+  },
+  {
+    "revision": "10c8a3d03e1534f13df1",
+    "url": "/Swe-Portfolio-Swe/static/js/2.3c1f8669.chunk.js"
+  },
+  {
+    "revision": "5f970f35d1b21881415c",
+    "url": "/Swe-Portfolio-Swe/static/js/3.435f9e5a.chunk.js"
+  },
+  {
+    "revision": "cbf326b96cb1d0bc2d95",
+    "url": "/Swe-Portfolio-Swe/static/js/main.d4c49e5e.chunk.js"
+  },
+  {
+    "revision": "28b068fb070ed9da7402",
+    "url": "/Swe-Portfolio-Swe/static/js/runtime~main.8d5acd5a.js"
+  },
+  {
+    "revision": "abd18e548910aee453f8537124eb2874",
+    "url": "/Swe-Portfolio-Swe/static/media/CentraNo2-Bold.abd18e54.ttf"
+  },
+  {
+    "revision": "090b88d28af7652bab33cb15566c58f5",
+    "url": "/Swe-Portfolio-Swe/static/media/CentraNo2-Book.090b88d2.ttf"
+  },
+  {
+    "revision": "180408241ad7ff00b6cea86ee805fec8",
+    "url": "/Swe-Portfolio-Swe/static/media/CentraNo2-Medium.18040824.ttf"
+  },
+  {
+    "revision": "91837c3008b520c0dfc0fc76454a9139",
+    "url": "/Swe-Portfolio-Swe/static/media/banner-bg.91837c30.png"
+  },
+  {
+    "revision": "50663b2b6a0fa3905c7f7aadd9ca1fd5",
+    "url": "/Swe-Portfolio-Swe/static/media/color-sharp.50663b2b.png"
+  },
+  {
+    "revision": "1173d6aa8f8bf1766978eda50cbd4c7b",
+    "url": "/Swe-Portfolio-Swe/static/media/color-sharp2.1173d6aa.png"
+  },
+  {
+    "revision": "bf09516b7a040f86b5d16b45a195d4aa",
+    "url": "/Swe-Portfolio-Swe/static/media/contact-img.bf09516b.svg"
+  },
+  {
+    "revision": "13c154355bb09606ef2052b6aacd5fc0",
+    "url": "/Swe-Portfolio-Swe/static/media/footer-bg.13c15435.png"
+  },
+  {
+    "revision": "e02c229832d776d895be48c40af8cdbc",
+    "url": "/Swe-Portfolio-Swe/static/media/header-img.e02c2298.svg"
+  },
+  {
+    "revision": "cd2c56e97e6a36b14e1effec171abc4b",
+    "url": "/Swe-Portfolio-Swe/static/media/logo.cd2c56e9.svg"
+  },
+  {
+    "revision": "e88903470cc4519673d4eaee800ef667",
+    "url": "/Swe-Portfolio-Swe/static/media/meter1.e8890347.svg"
+  },
+  {
+    "revision": "c8c8c40eb7771d483f78e0e77542b43f",
+    "url": "/Swe-Portfolio-Swe/static/media/meter2.c8c8c40e.svg"
+  },
+  {
+    "revision": "13a2ff8dc08442981a7a1e28dc50ee17",
+    "url": "/Swe-Portfolio-Swe/static/media/meter3.13a2ff8d.svg"
+  },
+  {
+    "revision": "ca6a763773140d9e67bf30f53fd9a2de",
+    "url": "/Swe-Portfolio-Swe/static/media/nav-icon1.ca6a7637.svg"
+  },
+  {
+    "revision": "974d1ea0eb5fe0830ebbb9a8e68a4c6d",
+    "url": "/Swe-Portfolio-Swe/static/media/nav-icon2.974d1ea0.svg"
+  },
+  {
+    "revision": "780b2af5df9fa4f7197948bd2aa107c9",
+    "url": "/Swe-Portfolio-Swe/static/media/nav-icon3.780b2af5.svg"
+  },
+  {
+    "revision": "c3188ccd40977075faf950466471c2a8",
+    "url": "/Swe-Portfolio-Swe/static/media/project-img1.c3188ccd.png"
+  },
+  {
+    "revision": "6e3a3867401097d2319805751c9f439b",
+    "url": "/Swe-Portfolio-Swe/static/media/project-img2.6e3a3867.png"
+  },
+  {
+    "revision": "9796734d864c0027849294deb7b92826",
+    "url": "/Swe-Portfolio-Swe/static/media/project-img3.9796734d.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Swe-Portfolio-Swe/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Swe-Portfolio-Swe/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Swe-Portfolio-Swe/static/media/revicons.2feb69cc.eot"
+  }
+]);
