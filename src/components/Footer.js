bb8d3ada009f26b971a846478425dcc960cf3762
Swetha Sebastian <swthsebastian@gmail.com>
2024-07-04 12:55:57 +0100

    updated social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ export const Footer = () => {
                     </Col>
                     <Col sm={10} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="www.linkedin.com/in/swethasebastian" target="_blank" rel="noopener noreferrer">
+                            <a href="https://www.linkedin.com/in/swethasebastian" target="_blank" rel="noopener noreferrer">
                                 <img src={navIcon1} alt="LinkedIn"/>
                             </a>
                             <a href="https://github.com/swedevswe" target="_blank" rel="noopener noreferrer">
